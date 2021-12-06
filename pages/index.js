a3c76c3534915a0cdf685fb32acdca259b4307ff
Noah Milberger <n.milberger@gmail.com>
2021-12-05 21:12:11 -0500

    Updated form to use Formik. Added Styling.

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,58 @@ import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { Formik, Form, useField } from "formik";
+import * as Yup from "yup";
 // import { Responder } from "./openai";
+
+const MyTextInput = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <input className="text-input" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
 
 export default function Home() {
   const [userInput, setUserInput] = useState("");
   const [toClassify, setToClassify] = useState("");
   const [response, setResponse] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+
+  const SignupForm = () => {
+    return (
+      <Formik
+        initialValues={{ userInput: "" }}
+        validationSchema={Yup.object({
+          userInput: Yup.string().required("Required"),
+        })}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            setToClassify(values.userInput);
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        <Form>
+          <MyTextInput
+            label="Sentiment Analysis"
+            name="userInput"
+            type="text"
+            placeholder="Enter Text"
+          />
+
+          <div>
+            <button type="submit">Submit</button>
+          </div>
+        </Form>
+      </Formik>
+    );
+  };
 
   useEffect(() => {
     const fetchResponse = async () => {
@@ -29,7 +74,7 @@ export default function Home() {
     };
 
     fetchResponse();
-    setToClassify("");
+    // setToClassify("");
   }, [toClassify]);
 
   function updateUserInput(e) {
@@ -37,22 +82,14 @@ export default function Home() {
     setUserInput(e.target.value);
   }
 
-  // function handleButtonPress() {
-  //   Responder(toClassify).then((value) =>
-  //     setResponse(value.data.choices[0].text)
-  //   );
-  // }
-
   return (
     <div>
-      <input
-        name="userInput"
-        onChange={updateUserInput}
-        value={userInput}
-        placeholder="Enter Text"
-      />
-      <button onClick={() => setToClassify(userInput)}>Enter</button>
-      {isLoading ? <div>Loading ...</div> : <div>{response.text}</div>}
+      <SignupForm />
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <div>{`${toClassify} - ${response.text}`}</div>
+      )}
     </div>
   );
 }
